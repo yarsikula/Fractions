f9f6f6c27ef229bec5abc1cab8db831cb124e4a5
solilto1 <solilto1@uhk.cz>
2025-02-17 11:28:09 +0100

    added math operations with fractions

--- src/main/java/TheFractions.java
+++ src/main/java/TheFractions.java
@@ -2,10 +2,50 @@ import cz.uhk.fractions.model.Fraction;
 
 public class TheFractions {
 
+    public static int isThereADivisor(Fraction a) {
+        int top = a.getTop();
+        int bottom = a.getBottom();
+        int divisor = 2;
+        int biggestDivisor = 1;
+
+        while (divisor <= top && divisor <= bottom) {
+            if (top%divisor == 0 && bottom%divisor == 0) {
+                biggestDivisor = divisor;
+            }
+            divisor++;
+        }
+        System.out.printf("Biggest divisor found: %d\n",biggestDivisor);
+        return biggestDivisor;
+    }
+
+    public static int isThereNeedForFraction(Fraction a) {
+        if (a.getTop()%a.getBottom() == 0) {return a.getTop() / a.getBottom();} else {System.out.println("This fraction can't be an integer"); return 0;}
+    }
+
     public static void main(String[] args) {
         //creating a fraction using a class
         Fraction a = new Fraction(3,4);
+        Fraction b = new Fraction(8,3);
 
-        System.out.printf("%s\n", a);
+        Fraction c = a.multiply(b);
+        System.out.printf("%s\n", c);
+
+        int divisor = isThereADivisor(c);
+        if (divisor > 1) {
+            c = c.shorten(divisor);
+            System.out.printf("After shortening %s\n", c);
+        }
+        int helpNumber = isThereNeedForFraction(c);
+        if (helpNumber != 0) {System.out.printf("Fraction can be expressed as just %d\n", helpNumber);}
+
+        c = a.divide(b);
+        System.out.printf("%s\n", c);
+
+        c = a.add(b);
+        System.out.printf("%s\n", c);
+
+        c = a.subtract(b);
+        System.out.printf("%s\n", c);
+
     }
 }
--- src/main/java/cz/uhk/fractions/model/Fraction.java
+++ src/main/java/cz/uhk/fractions/model/Fraction.java
@@ -12,6 +12,26 @@ public class Fraction {
     public int getTop() {return top;}
     public int getBottom() {return bottom;}
 
+    public Fraction multiply(Fraction second) {
+        return new Fraction(top * second.top, bottom * second.bottom);
+    }
+
+    public Fraction divide(Fraction second) {
+        return new Fraction(top * second.bottom, bottom * second.top);
+    }
+
+    public Fraction add(Fraction second) {
+        return new Fraction((top * second.bottom) + (second.top * bottom), bottom * second.bottom);
+    }
+
+    public Fraction subtract(Fraction second) {
+        return new Fraction((top * second.bottom) - (second.top * bottom), bottom * second.bottom);
+    }
+
+    public Fraction shorten(int divisor) {
+        return new Fraction(top / divisor, bottom / divisor);
+    }
+
     @Override
     public String toString() {
         return String.format("%d/%d", top, bottom);
